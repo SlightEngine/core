22d9ba42d79ce9c5b93a3e539deda4c1a6c654b2
Sea Grapes <natuloid@gmail.com>
2022-05-21 16:25:47 -0700

    checkin

--- src/core/Camera.js
+++ src/core/Camera.js
@@ -1,3 +1,7 @@
-export default function Camera() {
+import { Transform } from "../../ogl/src/core/Transform";
+import { Mat4 } from "../../ogl/src/math/Mat4";
+import { Vec3 } from "../../ogl/src/math/Vec3";
+
+export function Camera() {
   
 }
